6117ce13ac645987748916547b7f8aa8bafc9bd6
TzRain <56442462+TzRain@users.noreply.github.com>
2020-02-22 01:08:13 +0800

    2020-2-22

--- routes/admin/task/init.js
+++ routes/admin/task/init.js
@@ -6,22 +6,27 @@ const path = "/admin/task/init"
 exports.route = {
     async get() {
         console.log("正在访问>>>>>>>>>" + path + "<<<<<<<<<<");
-        let taskdb = await mongodb("task")
-        let task = []
-        taskdb.remove()
-        task.push({
-            taskNum: "11",
-            point: 4,
-            limt: [60 * 5, 60 * 6 + 30, 60 * 7]
-        })
-        task.push({
-            taskNum: "12",
-            point: 4,
-            limt: [60 * 10, 60 * 11]
-        })
-        for(x in task){
-            await taskdb.insertOne(task[x])
+        try{
+            let taskdb = await mongodb("task")
+            let task = []
+            taskdb.remove()
+            task.push({
+                taskNum: "11",
+                point: 4,
+                limt: [60 * 5, 60 * 6 + 30, 60 * 7]
+            })
+            task.push({
+                taskNum: "12",
+                point: 4,
+                limt: [60 * 10, 60 * 11]
+            })
+            for(x in task){
+                await taskdb.insertOne(task[x])
+            }
+        }catch(e){
+            console.log(e);
+            return "初始化失败"    
         }
-        return task
+        return "初始化成功"
     }
 }

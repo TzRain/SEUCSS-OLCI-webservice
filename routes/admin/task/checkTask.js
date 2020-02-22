f1d0a95b0995c79b3527adbcc47630920285f99a
TzRain <56442462+TzRain@users.noreply.github.com>
2020-02-22 22:55:31 +0800

    2020-2-22

--- routes/admin/task/checkTask.js
+++ routes/admin/task/checkTask.js
@@ -1,7 +1,7 @@
 const mongodb = require('../../../database/mongodb')
 const ObjectId = require('mongodb').ObjectId
 
-const path = "/user/sumbit/checkTask"
+const path = "/admin/task/checkTask"
 
 exports.route = {
 	async get({ QQ, taskNum }) {
@@ -9,32 +9,16 @@ exports.route = {
 		let userdb = await mongodb('user')
 		let taskdb = await mongodb('task')
 		let time = new Date()
-		let res="打卡成功"
+		let res="打卡成功!"
 		try {
-			let time = new Date()
-			let minute = time.getUTCHours() * 60 + time.getUTCMinutes()
-
 			let task = await taskdb.findOne({ taskNum })
 			let { limt, point } = task
-			console.log(task);
 
 			let user = await userdb.findOne({ QQ })
 			let { rating, doneList, doneListToday } = user
-			console.log(user);
-
-			if (minute < limt[0]) return "还没有开始呢"
-			if (limt.slice(-1) < minute) return "今天已经结束咯"
 
 			for (let x in doneListToday) {
 				if (doneListToday[x] == taskNum) return "今天以及打过卡咯"
-				console.log("num=" + num);
-				console.log("tasknum=" + taskNum);
-			}
-
-			for (let x in limt) {
-				console.log(limt[x])
-				if (minute < limt[x]) break
-				point--
 			}
 
 			rating += point
@@ -43,7 +27,7 @@ exports.route = {
 			
 			await userdb.updateOne({ QQ }, { $set: { doneList, doneListToday, rating } })
 			
-			res+="获得"+point.toString()+"积分"
+			res+=QQ+"获得"+point.toString()+"积分"
 
 		} catch (e) {
 			console.log(e)

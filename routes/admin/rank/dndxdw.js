3d7cf22a3599c8c1f5ae10dd9ebe7e7f80a5d258
HHHKKKHHH <1790733272@qq.com>
2020-04-01 23:33:25 +0800

    dw

---
+++ routes/admin/rank/dndxdw.js
@@ -0,0 +1,38 @@
+const mongodb = require('../../../database/mongodb')
+
+exports.route = {
+    async get() {
+        let userdb = await mongodb("user")
+        let users = await userdb.find().toArray()
+        let dw = [];
+        let statistic = {
+            dw: {
+                point: 0,
+                person: 0,
+                average: 0
+            },
+            total: {
+                point: 0,
+                person: 0,
+                average: 0
+            }
+        }
+        users.forEach(user => {
+            statistic.total.point += user.point
+            statistic.total.person++
+            if (user.name.includes("董导应援团")) {
+                dw.push({
+                    name: user.name,
+                    cardnum: user.cardnum,
+                    point: user.point,
+                })
+                statistic.dw.point += user.point
+                statistic.dw.person++
+            }
+        })
+        return {
+            statistic,
+            dw
+        }
+    }
+}
